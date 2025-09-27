10df16ef1199a34660b315aa14d6ba603e477b84
EngSp00ner <engsp00ner51@gmail.com>
2025-09-28 00:18:47 +0300

    adding filter to my invoicelist

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -3,8 +3,10 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
-  function (Controller, JSONModel, formatter) {
+  function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
@@ -12,11 +14,25 @@ sap.ui.define(
        * @override
        */
       formatter: formatter,
-      onInit() {
+      onInit: function () {
         const oViewModel = new JSONModel({
           currency: "EUR",
         });
         this.getView().setModel(oViewModel, "view");
+      },
+
+      onFilterInvoices(oEvent) {
+        const aFilter = [];
+        const sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+          aFilter.push(
+            new Filter("ProductName", FilterOperator.Contains, sQuery)
+          );
+        }
+        //filter binding
+        const oList = this.byId("invoicesList");
+        const oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter); // apply filter to the list
       },
     });
   }
